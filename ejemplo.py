077244c3ccb4d2945a2159fcd22d6641c8980f97
Alfredo Villagran Olguin <avillagran@ipn.mx>
2026-04-19 13:34:43 -0600

    Cargar desde un archivo los datos de UN alumno en UN diccionario

--- ejemplo.py
+++ ejemplo.py
@@ -3,7 +3,7 @@ from subprocess import run
 if __name__== '__main__':
     run('cls', shell=True)
 
-    propiedades = ['Nombre', 'Paterno', 'Materno', 'Boleta', 'Semestre', 'Promedio', 'Carrera']
+    listaDePropiedades = ['Nombre', 'Paterno', 'Materno', 'Boleta', 'Semestre', 'Promedio', 'Carrera']
     alumno = {}
 
     # Se muestra el alumno como un diccionario vacio
@@ -12,14 +12,14 @@ if __name__== '__main__':
     print()
 
     # Se genera un diccionario con la lista de propiedades como claves, y los valores ingresados por el usuario
-    for clave in propiedades:
+    for clave in listaDePropiedades:
         valor = input(f'Ingresa tu {clave} ')
         alumno.update( {clave : valor} )
     print()
 
     # Se muestra la lista de propiedades
-    print('propiedades', type(propiedades) )
-    print( propiedades )
+    print('listaDePropiedades', type(listaDePropiedades) )
+    print( listaDePropiedades )
     print()
 
     # Se muestra el alumno como un diccionario
@@ -48,3 +48,42 @@ if __name__== '__main__':
 
 
     ArchivoEscritura.close()
+
+    # Si los datos de UN alumno estan guardados en agenda.txt, 
+    # se genera un diccionario en el que se cargaran
+    diccionario = {}
+
+    # Se muestra el diccionario vacio
+    print('diccionario', type(diccionario) )
+    print( diccionario )
+    print()
+    
+    # Se cargan los datos de UN alumno, desde el archivo donde se encuentran
+    ArchivoLectura = open('agenda.txt', 'r')
+
+    # Se carga una línea y se le quita el salto de linea con strip('\n')
+    linea = ArchivoLectura.readline()
+    cadenaSinSalto = linea.strip('\n')
+
+    # Se parte la cadena en una lista de cadenas que estan unidas/separadas por coma
+    listaDeCadenas = cadenaSinSalto.split(',')
+
+    # Como la cantidad de cadenas en la lista corresponde a la cantidad de propiedades en la lista de propiedades
+    # se genera el diccionario con la misma posicion en cada lista
+    n = len(listaDePropiedades)
+    for posicion in range(n):
+        diccionario.update( { listaDePropiedades[posicion] : listaDeCadenas[posicion]} )
+
+    # Se muestra el diccionario generado con las propiedades deseadas y los datos cargados
+    print('diccionario', type(diccionario) )
+    print( diccionario )
+    print()
+    
+    # Se muestra cada pareja clave:valor (items) del diccionario 
+    print('diccionario.items()', type(diccionario.items()) )
+    for pareja in diccionario.items():
+        print( f'{pareja[0]} : {pareja[1]}' )
+    print()
+
+    # Se cierra el archivo, cuando ya no se necesita
+    ArchivoLectura.close()
